36cd880d0d92ed5232ae5fa2a93443b8b0d7d469
Michael Barron <michael.barron08@gmail.com>
2021-08-02 21:13:20 -0500

    commit

---
+++ arrays.rb
@@ -0,0 +1,32 @@
+a = [1,2,10,3,4,5,6,7,8,9]
+
+b = ['a','b','c','d']
+
+
+
+
+
+
+
+# puts a.fetch(3)
+
+# puts a.delete(8)
+# puts a
+
+# puts b.reverse
+
+# puts a.length
+
+# puts a.sort
+
+# puts a.slice(3,7)
+
+# puts a.shuffle
+
+# puts a.join "-"
+
+# puts b.insert(3,'e')
+# puts b
+
+
+# puts a.values_at(1,3).join('and')
